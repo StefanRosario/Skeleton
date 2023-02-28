e725a2a3b6b6097adaf2025aadb1e05d4550df94
p2668852 <p2668852@GWHXX471WIN019W>
2023-02-28 14:53:28 +0000

    Added all the testing for all the names.

--- Testing2/tstAddress.cs
+++ Testing2/tstAddress.cs
@@ -11,22 +11,89 @@ namespace Testing2
         private static void InstanceOK()
         {
             //create an instance of the class we want to create
-            clsAddress AnAddress = new clsAddress();
+            clsStaff AnAddress = new clsStaff();
             //test to see that it exists
             Assert.IsNotNull(AnAddress);
             }
 
         [TestMethod]
-        public void ActivePropertyOK()
+        public void StaffIDPropertyOK()
         {
             //create an instance of the class we want to create
-            clsAddress AnAddress = new clsAddress();
+            clsStaff AStaff = new clsStaff();
+            //create some test data
+            Int32 TestData = 1;
+            //assign the data to the property
+            AStaff.StaffID = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AStaff.StaffID, TestData);
+        }
+
+        [TestMethod]
+        public void UsernamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data
+            string TestData = "johnsmith";
+            //assign the data to the property
+            AStaff.Username = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AStaff.Username, TestData);
+        }
+
+        [TestMethod]
+        public void PasswordPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data
+            string TestData = "password123";
+            //assign the data to the property
+            AStaff.Password = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AStaff.Password, TestData);
+        }
+
+        [TestMethod]
+        public void RolePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data
+            string TestData = "Manager";
+            //assign the data to the property
+            AStaff.Role = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AStaff.Role, TestData);
+        }
+
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AStaff.DateAdded = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AStaff.DateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void IsAuthorizedPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
             //create some test data
             Boolean TestData = true;
             //assign the data to the property
-            AnAddress.Active = TestData;
+            AStaff.IsEnabled = TestData;
             //test to see if the two values are the same
-            Assert.AreEqual(AnAddress.Active, TestData);
-        }
+            Assert.AreEqual(AStaff.IsEnabled, TestData);
         }
     }
+
+
+}
