86b5c0f8760be1c4e7fd260f8f56783eb5cd2058
Raphael890 <Raphael.akintunde890@gmail.com>
2023-03-01 17:00:11 +0000

    Added testing for order class.

---
+++ Testing3/tstOrder.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+
+            Assert.IsNotNull(AnOrder);
+            {
+            }
+        }
+    }
+}
