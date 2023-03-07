fedd14f3756b799c2d5990dffff351879af14530
p2668852 <p2668852@GWHXX471WIN019W>
2023-03-07 10:26:25 +0000

    Edited the code in the data entry to make it work.

--- AdminSystem/StaffManagementViewer.aspx.cs
+++ AdminSystem/StaffManagementViewer.aspx.cs
@@ -10,11 +10,5 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //Create a new instance of clsAddress
-        clsAddress AnAddress = new clsAddress();
-        //Get the data from the session object
-        AnAddress = (clsAddress)Session["AnAddress"];
-        //Display the house number for this entry
-        Response.Write(AnAddress.HouseNo);
     }
 }
