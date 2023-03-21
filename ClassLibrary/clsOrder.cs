4f03137fdf2f42b0d5db29d9abee40faf010c987
Raphael890 <Raphael.akintunde890@gmail.com>
2023-03-21 09:22:37 +0000

    Added find method to nearly all the tests.

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -7,28 +7,64 @@ namespace ClassLibrary
         //private data member for the address no property
         private Int32 mCostumerID;
         private string mShippingAddress;
-        
-        
+        private DateTime mOrderDate;
+        private decimal mTotalCost;
+        private Boolean mIsShipped;
+        private Int32 mOrderID;
+
+
+
 
 
         public clsOrder()
         {
         }
 
-        public int OrderID { get; set; }
-        public DateTime DateAdded { get; set; }
-        public DateTime OrderDate { get; set; }
+        public int OrderID
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mOrderID;
+            }
+
+            set
+            {
+                //this line of code allows data into the property
+                mOrderID = value;
+            }
+        }
+        public DateTime DateAdded
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mOrderDate;
+            }
+
+            set
+            {
+                //this line of code allows data into the property
+                mOrderDate = value;
+            }
+        }
+        public DateTime OrderDate {
+            get
+            {
+                return OrderDate;
+            }
+
+            set
+            {
+                OrderDate = value;
+            }
+        }
         public string ShippingAddress
         {
-
             get
-
             {
                 return mShippingAddress;
             }
-
-
-
             set
             {
                 mShippingAddress = value;
@@ -48,12 +84,39 @@ namespace ClassLibrary
                 mCostumerID = value;
             }
         }
-        public decimal TotalCost { get; set; }
-        public bool IsEnabled { get; set; }
+        public decimal TotalCost
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mTotalCost;
+            }
+
+            set
+            {
+                //this line of code allows data into the property
+                mTotalCost = value;
+            }
+        }
+        public bool IsEnabled
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mIsShipped;
+            }
+
+            set
+            {
+                //this line of code allows data into the property
+                mIsShipped = value;
+            }
+        }
 
         public bool Find(int costumerID)
         {
             mCostumerID = 21;
+           
             //always return true
             return true;
         }
@@ -64,5 +127,25 @@ namespace ClassLibrary
             //always return true
             return true;
         }
+
+        public bool Find(decimal totalCost)
+        {
+            mTotalCost = 1;
+            return true;
+        }
+
+        public bool Find(DateTime orderDate)
+        {
+
+            mOrderDate = Convert.ToDateTime("30/11/2022");
+            //always return true
+            return true;
+        }
+
+
+
     }
-}
+    }
+    
+
+    
