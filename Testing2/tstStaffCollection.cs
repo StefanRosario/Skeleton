521d3df93787f99625530580888bbb12324d7f1a
p2668852 <p2668852@GWHXX471WIN019W>
2023-04-11 15:33:12 +0100

    Started creating the collection class.

---
+++ Testing2/tstStaffCollection.cs
@@ -0,0 +1,36 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStaffCollection
+    {
+        [TestMethod]
+        public void AddressListOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection AllStaffs = new clsStaffCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsStaff> TestList = new List<clsStaff>();
+            //add an item to the list
+            //create the item of test data
+            clsStaff TestItem = new clsStaff();
+            //set its properties
+            TestItem.Username = "miguelcorachea";
+            TestItem.Password = "abcd123";
+            TestItem.Role = "Customer Manager";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Active = true;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllStaffs.StaffList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStaffs.StaffList, TestList);
+        }
+    }
+}
