1a884912c84788969b285d50a80de471cc90af29
Raphael890 <Raphael.akintunde890@gmail.com>
2023-04-15 18:32:09 +0100

    continued with order collection

--- ClassLibrary/clsOrderCollection.cs
+++ ClassLibrary/clsOrderCollection.cs
@@ -1,9 +1,37 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 
 namespace ClassLibrary
 {
     public class clsOrderCollection
     {
+        //private data member for this list
+        List<clsOrder> mOrderList = new List<clsOrder>();
+        public List<clsOrder> OrderList
+        {
+            get
+            {
+                //set the private data
+                return mOrderList;
+            }
+            set
+            {
+                //set the private data
+                mOrderList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                //set the private data
+                return mOrderList.Count;
+            }
+            set
+            {
+
+            }
+        }
     }
 }
+    
--- Testing3/tstOrderCollection.cs
+++ Testing3/tstOrderCollection.cs
@@ -1,11 +1,64 @@
 ﻿using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace Testing3
 {
     [TestClass]
     public class tstOrderCollection
     {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create instance of the class we want tot create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //test to see if it exists
+            Assert.IsNotNull(AllOrders);
+
+        }
+        [TestMethod]
+        public void OrderListOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsOrder> TestList = new List<clsOrder>();
+            //add an item to the list
+            //create the item of test data
+            clsOrder TestItem = new clsOrder();
+            //set its properties
+            TestItem.OrderID = 1;
+            TestItem.CustomerID = 1;
+            TestItem.TotalCost = Convert.ToDecimal("65.00");
+            TestItem.ShippingAddress = "60, Finsubry road, Leicester, LE2 6FU";
+            TestItem.OrderDate = Convert.ToDateTime("22/02/2023");
+            TestItem.Shipped = false;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllOrders.OrderList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrders.OrderList, TestList);
+        }
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create some test data to assign to the property
+            Int32 SomeCount = 0;
+            //assign the data to the property
+            AllOrders.Count = SomeCount;
+            //Test to see that the two values are the same
+            Assert.AreEqual(AllOrders.Count, SomeCount);
+
+        }
+        
+
+       
+        }
     }
-}
+
+
