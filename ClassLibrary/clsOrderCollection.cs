438d5f2f4bea986f8c2b1c8b70f6b5b27e52b349
Raphael890 <Raphael.akintunde890@gmail.com>
2023-04-18 16:07:32 +0100

    continued to last week

--- ClassLibrary/clsOrderCollection.cs
+++ ClassLibrary/clsOrderCollection.cs
@@ -49,5 +49,104 @@ namespace ClassLibrary
                 mThisOrder = value;
             }
         }
+              void PopulateArray(clsDataConnection DB)
+        {
+            //populates the array list based on the data table in the parameter DB
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount;
+            //get the count of records
+            RecordCount = DB.Count;
+            //clear the private array list
+            mOrderList = new List<clsOrder>();
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank address
+                clsOrder AnOrder = new clsOrder();
+                //read in the fields from the current record
+                AnOrder.OrderID = Convert.ToInt32(DB.DataTable.Rows[0]["OrderID"]);
+                AnOrder.CustomerID = Convert.ToInt32(DB.DataTable.Rows[0]["CustomerID"]);
+                AnOrder.OrderDate = Convert.ToDateTime(DB.DataTable.Rows[0]["OrderDate"]);
+                AnOrder.TotalCost = Convert.ToDecimal(DB.DataTable.Rows[0]["TotalCost"]);
+                AnOrder.Shipped = Convert.ToBoolean(DB.DataTable.Rows[0]["Shipped"]);
+                AnOrder.ShippingAddress = Convert.ToString(DB.DataTable.Rows[0]["ShippingAddress"]);
+
+                //add the record to the private data member
+                mOrderList.Add(AnOrder);
+                //point at the next record
+                Index++;
+            }
+        }
+
+        public clsOrderCollection()
+        {
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_OrderProcess_SelectAll");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
+
+        public int Add()
+        {
+            //adds a new record ot the database based on the values of thisStaff
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@CustomerID", mThisOrder.CustomerID);
+            DB.AddParameter("@TotalCost", mThisOrder.TotalCost);
+            DB.AddParameter("@ShippingAddress", mThisOrder.ShippingAddress);
+            DB.AddParameter("@OrderDate", mThisOrder.OrderDate);
+            DB.AddParameter("@Shipped", mThisOrder.Shipped);
+            //execute the query returning the primary key value
+            return DB.Execute("sproc_OrderProcess_Insert");
+
+        }
+
+        public void Update()
+        {
+            //update an existing record based on the values op ThisStaff
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored prodecure
+            DB.AddParameter("@OrderID", mThisOrder.OrderID);
+            DB.AddParameter("@CustomerID", mThisOrder.CustomerID);
+            DB.AddParameter("@TotalCost", mThisOrder.TotalCost);
+            DB.AddParameter("@ShippingAddress", mThisOrder.ShippingAddress);
+            DB.AddParameter("@OrderDate", mThisOrder.OrderDate);
+            DB.AddParameter("@Shipped", mThisOrder.Shipped);
+            //execute the stored procedure
+            DB.Execute("sproc_OrderProcess_Update");
+        }
+
+        public void Delete()
+        {
+            //deletes the record pointed to by thisStaff
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@OrderID", mThisOrder.OrderID);
+            //execute the stored procedure
+            DB.Execute("sproc_OrderProcess_Delete");
+        }
+
+        public void ReportByTotalCost(string TotalCost)
+        {
+            //filters the record based on a full or partial Role
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //send the Role parameter to the database
+            DB.AddParameter("@TotalCost", TotalCost);
+            //execute the stored procedure
+            DB.Execute("sproc_OrderProcess_FilterByTotalCost");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
     }
 }
+     
+
+
--- Testing3/tstOrderCollection.cs
+++ Testing3/tstOrderCollection.cs
@@ -79,5 +79,154 @@ namespace Testing3
             //test to see that the two values are the same
             Assert.AreEqual(AllOrders.Count, TestList.Count);
         }
+        [TestMethod]
+        public void AddMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create the item of test data
+            clsOrder TestItem = new clsOrder();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.OrderID = 1;
+            TestItem.CustomerID = 1;
+            TestItem.TotalCost = Convert.ToDecimal("65.00");
+            TestItem.ShippingAddress = "60, Finsubry road, Leicester, LE2 6FU";
+            TestItem.OrderDate = Convert.ToDateTime("22/02/2023");
+            TestItem.Shipped = false;
+            //set ThisStaff to the test data
+            AllOrders.ThisOrder = TestItem;
+            //add the record
+            PrimaryKey = AllOrders.Add();
+            //set the primary key of the test data
+            TestItem.OrderID = PrimaryKey;
+            //find the record
+            AllOrders.ThisOrder.Find(PrimaryKey);
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrders.ThisOrder, TestItem);
+        }
+
+        [TestMethod]
+        public void UpdateMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create the item of test data
+            clsOrder TestItem = new clsOrder();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.CustomerID = 1;
+            TestItem.TotalCost = Convert.ToDecimal("65.00");
+            TestItem.ShippingAddress = "60, Finsubry road, Leicester, LE2 6FU";
+            TestItem.OrderDate = Convert.ToDateTime("22/02/2023");
+            TestItem.Shipped = false;
+            //set ThisStaff to the test data
+            AllOrders.ThisOrder = TestItem;
+            //add the record
+            PrimaryKey = AllOrders.Add();
+            //set the primary key of the test data
+            TestItem.OrderID = PrimaryKey;
+            //modify the test data
+            TestItem.CustomerID = 1;
+            TestItem.TotalCost = Convert.ToDecimal("65.00");
+            TestItem.ShippingAddress = "60, Finsubry road, Leicester, LE2 6FU";
+            TestItem.OrderDate = Convert.ToDateTime("22/02/2023");
+            TestItem.Shipped = false;
+            //set the record based on the new test data
+            AllOrders.ThisOrder = TestItem;
+            //Update the record
+            AllOrders.Update();
+            //find the record
+            AllOrders.ThisOrder.Find(PrimaryKey);
+            //test to see ThisStaff matches the test data
+            Assert.AreEqual(AllOrders.ThisOrder, TestItem);
+        }
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create the item of test data
+            clsOrder TestItem = new clsOrder();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.CustomerID = 1;
+            TestItem.TotalCost = Convert.ToDecimal("65.00");
+            TestItem.ShippingAddress = "60, Finsubry road, Leicester, LE2 6FU";
+            TestItem.OrderDate = Convert.ToDateTime("22/02/2023");
+            TestItem.Shipped = false;
+            //set ThisStaff to the test data
+            AllOrders.ThisOrder = TestItem;
+            //add the record
+            PrimaryKey = AllOrders.Add();
+            //set the primary key of the test data
+            TestItem.OrderID = PrimaryKey;
+            //find the record
+            AllOrders.ThisOrder.Find(PrimaryKey);
+            //delete the record
+            AllOrders.Delete();
+            //now find the record
+            Boolean Found = AllOrders.ThisOrder.Find(PrimaryKey);
+            //test to see that the record was not found
+            Assert.IsFalse(Found);
+        }
+        [TestMethod]
+        public void ReportByTotalCostMethodOK()
+        {
+            //create an instance of the class containing unfiltered results
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create an instance of the filtered data
+            clsOrderCollection FilteredOrders = new clsOrderCollection();
+            //apply a blank string (should return all records);
+            FilteredOrders.ReportByTotalCost("");
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrders.Count, FilteredOrders.Count);
+        }
+        [TestMethod]
+        public void ReportByTotalCoNoneFound()
+        {
+            //create an instance of the filtered data
+            clsOrderCollection FilteredOrders = new clsOrderCollection();
+            //apply a TotalCost that doesn't exist
+            FilteredOrders.ReportByTotalCost("65.00");
+            //test to see that there are no records
+            Assert.AreEqual(0, FilteredOrders.Count);
+        }
+
+        [TestMethod]
+        public void ReportByTotalCostTestDataFound()
+        {
+            //create an instance of the filtered data
+            clsOrderCollection FilteredOrders = new clsOrderCollection();
+            //var to store outcome
+            Boolean OK = true;
+            //apply a TotalCost that doesn't exist
+            FilteredOrders.ReportByTotalCost("65.00");
+            //check that the correct number of records are found
+            if (FilteredOrders.Count == 2)
+            {
+                //check that the first record is ID 4
+                if (FilteredOrders.OrderList[0].OrderID != 4)
+                {
+                    OK = false;
+                }
+                //check that the first record is ID 22
+                if (FilteredOrders.OrderList[1].OrderID != 22)
+                {
+                    OK = false;
+                }
+
+                else
+                {
+                    OK = false;
+                }
+                //test to see that there are no records
+                Assert.IsTrue(OK);
+            }
+        }
+
     }
 }
