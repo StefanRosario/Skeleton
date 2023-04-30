5e6bb84877f1302b4d0494fbdd38b37233e5e76e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-04-30 21:02:25 +0100

    almost dw

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -12,6 +12,8 @@ public partial class _1_DataEntry : System.Web.UI.Page
     //variable to store the primary key with page level scope
     Int32 CustomerId;
 
+    public object Phone { get; private set; }
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -22,13 +24,9 @@ public partial class _1_DataEntry : System.Web.UI.Page
         //Create a new instance of clsStaff
         clsCustomer ACustomer = new clsCustomer();
         //capture Customer Id
-        string CustomerId = CustomerIdTxt.Text;
-        //Capture the FullName 
         string FullName = FullNameTxt.Text;
         //Capture the Email
-        string Email = EmailAddressTxt.Text;
-        //capture Phone
-        string Phone = PhoneNumberTxt.Text;
+        string Email = EmailTxt.Text;
         //Capture the date Registered
         string DateRegistered = DateRegisterTxt.Text;
         //capture shipping address
@@ -36,32 +34,33 @@ public partial class _1_DataEntry : System.Web.UI.Page
         //Variable to store any error messages
         string Error = "";
         //Validate the data
-        Error = ACustomer.Valid(FullName, Email, Phone, DateRegistered);
+        Error = ACustomer.Valid(FullName, Email, ShippingAddress, DateRegistered);
         if (Error == "")
         {
-            //capture the StaffID
+            //capture the CustomerId
             ACustomer.CustomerId = Convert.ToInt32(CustomerId);
-            //Capture the Username
+            //Capture the FullName
             ACustomer.FullName = FullName;
-            //Capture the Password
+            //Capture the Email
             ACustomer.Email = Email;
-            //Capture the Role
+            //Capture the Phone
             ACustomer.Phone = Convert.ToInt32(Phone);
             //capture shipping address
             ACustomer.ShippingAddress = ShippingAddress;
-            //Capture DateAdded
+            //Capture DAte Registered
             ACustomer.DateRegistered = Convert.ToDateTime(DateRegistered);
-            //capture active
-            
+        
             //create a new instance of the address collection
             clsCustomerCollection CustomerList = new clsCustomerCollection();
 
-            //set the ThisStaff property
+            //set the ThisCustomer property
             CustomerList.ThisCustomer = ACustomer;
 
             //if this is a new record i.e. StaffID = -1 then add the data
-            if (ACustomer == -1)
+            if (CustomerId == -1)
             {
+                //set the ThisStaff property
+                CustomerList.ThisCustomer = ACustomer;
                 //add the new record
                 CustomerList.Add();
             }
@@ -69,7 +68,9 @@ public partial class _1_DataEntry : System.Web.UI.Page
             else
             {
                 //find the record to update
-                CustomerList.ThisCustomer.Find(ACustomer);
+                CustomerList.ThisCustomer.Find(CustomerId);
+                //set the ThisStaff property
+                CustomerList.ThisCustomer = ACustomer;
                 //update the record
                 CustomerList.Update();
             }
@@ -107,25 +108,26 @@ public partial class _1_DataEntry : System.Web.UI.Page
     protected void btnFind_Click(object sender, EventArgs e)
     {
         //Create am instance of the address class
-        clsStaff AStaff = new clsStaff();
+        clsCustomer ACustomer = new clsCustomer();
         //Variable to store the primary key
-        Int32 StaffID;
+        Int32 CustomerId;
         //Variable to store the result of the find operation
         Boolean Found = false;
         //Get the primary key entered by the user
-        StaffID = Convert.ToInt32(txtStaffID.Text);
+        CustomerId = Convert.ToInt32(CustomerIdTxt.Text);
         //Find the record
-        Found = AStaff.Find(StaffID);
+        Found = ACustomer.Find(CustomerId);
         //If found
         if (Found == true)
         {
             //Display the values of the properties in the form
-            txtStaffID.Text = AStaff.StaffID.ToString();
-            txtUsername.Text = AStaff.Username.ToString();
-            txtPassword.Text = AStaff.Password.ToString();
-            txtRole.Text = AStaff.Role.ToString();
-            txtDateAdded.Text = AStaff.DateAdded.ToString();
-            chkActive.Checked = AStaff.Active;
+            CustomerIdTxt.Text = ACustomer.CustomerId.ToString();
+            FullNameTxt.Text = ACustomer.FullName.ToString();
+            EmailTxt.Text = ACustomer.Email.ToString();
+            PhoneNumberTxt.Text = ACustomer.Phone.ToString();
+            ShippingAddressTxt.Text = ACustomer.ShippingAddress.ToString();
+            DateRegisterTxt.Text = ACustomer.DateRegistered.ToString();
+            
         }
 
     }
